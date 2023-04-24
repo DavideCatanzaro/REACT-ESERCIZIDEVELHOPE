230fd9367b1f56e719e0001addab9761c75e48fc
DavideCatanzaro <dadocata03@gmail.com>
2023-04-24 12:19:11 +0200

    DONE

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Welcome } from './Props-Passing`name`prop';
 import {Hello} from './esercizio1';
 import { Welcome2 } from './Props-Defaultvalue';
+import { WelcomeAge } from './Props-Secondprop';
 function App() {
   return (
     <div>
       <Hello />
       <Welcome name={"Jimmy"}/>
       <Welcome2 />
+      <WelcomeAge  name={"DADO"} age={22} />
     </div>
   );
 }
