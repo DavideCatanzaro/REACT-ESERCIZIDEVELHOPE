89c00bec08f67372e9ab3a76445657eb017a635c
DavideCatanzaro <dadocata03@gmail.com>
2023-05-10 10:00:21 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
-import { Welcome } from './Props-Passing`name`prop';
-import {Hello} from './esercizio1';
-import { Welcome2 } from './ConditionalRendering-IfisequaltoJohn';
-import { WelcomeAge } from './Props-Secondprop';
-import { Counter } from './CreateACounterClassComponent';
-import { Counter2 } from './CounterProps';
-import { Counter3 } from './CounterWithComponentDidMount';
-import ClickCounter from './Events-ClickCounter';
-import ClickTracker from './Events-ClickTracker';
-import InteractiveWelcome from './InteractiveWelcomeComponent';
-import Login from './login';
-import Login2 from './resetLogIn';
-import UncontrolledLogin from './UncontrolledLogin'
-import TodoList from './TodoListComponent';
-import AddTodo from './AddTodo';
-import AddTodo2 from './ResetToDo';
-import RemoveTodo from './removeToDo';
-import RemoveTodo2 from './ToDoRenderProps';
-import { CounterFunc } from './counterFunction';
-import  { CounterFunc2 } from './sideEffectCounter';
-import { GithubUser } from './GithubUser';
+import { LanguageContext } from "./LanguageContext";
+import DisplayLanguage from "./components/displayLanguage";
+import { useState } from "react";
 
 function App() {
+  
+  const [language, setLanguage] = useState('en')
+  
+  function handleChange(event){
+    setLanguage(event.target.value)
+  }
+  
   return (
     <div>
-      {/* <Hello />
-      <Welcome2 age={65}/>
-      <WelcomeAge  name={"DADO"} age={22} />
-      <Counter />
-      <Counter2 initialValue={2} incrementBy={2}/>
-      <Counter3 />
-      <ClickCounter/>
-      <ClickTracker/>
-      <InteractiveWelcome/>
-      <Login/>
-      <Login2/> 
-      <UncontrolledLogin/>
-      <TodoList/> */}
-      <CounterFunc/>
-      <RemoveTodo2/>
-      <GithubUser />
-
+      <select onChange={handleChange} value={language}>
+        <option value='it'>ITALIANO</option>
+        <option value='en'>INGLESE</option>
+      </select>
+      <LanguageContext.Provider value={language}>
+        <DisplayLanguage />
+      </LanguageContext.Provider>
     </div>
   );
 }
