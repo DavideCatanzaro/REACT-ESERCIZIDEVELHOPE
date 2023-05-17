0f9f6cd9c1018f953ea56fd64291c5410fb13890
DavideCatanzaro <dadocata03@gmail.com>
2023-05-17 23:43:28 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { LanguageContext } from "./LanguageContext";
 import CarDetails from "./components/CarDetails";
 import GithubUser from "./components/CustomHook/GitHubUser";
+import Filteredlist from "./components/FiltredList";
 import DisplayLanguage from "./components/displayLanguage";
 import { useState } from "react";
 
@@ -17,10 +18,30 @@ function App() {
     year : 2010,
     color : "red"
   }
+
+  const person = [
+    {
+      name: "davide",
+      id: 1,
+      age: 19,
+    },
+    {
+      name: "fabio",
+      id: 2,
+      age: 15,
+    },
+    {
+      name: "fabiola",
+      id: 3,
+      age: 21,
+    },
+  ];
+
   return (
     <div>
       <GithubUser username='DavideCatanzaro'/>
       <CarDetails initialData={car}/>
+      <Filteredlist list={person}/>
     </div>
   );
 }
